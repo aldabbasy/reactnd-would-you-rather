36504b563da27d06437563af63084339325fa9b4
AL-Laith Dababseh <45473839+aldabbasy@users.noreply.github.com>
2020-12-20 22:27:23 +0200

    Modified Routes.js

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -9,25 +9,27 @@ import NavBar from "./NavBar"
 import Leaderboard from "./Leaderboard"
 import NewQuestion from "./NewQuestion"
 import Poll from "./Poll"
-import NotFound from "./NotFound"
+import NotFound from "../Screens/NotFound"
 
 function Routes({ isLogged }) {
-  return <div className="container">
-    <Switch>
-      {
-        !isLogged ? <Route path='/' exact component={Login}/> :
-          <Fragment>
-            <NavBar />
-            <Route path='/' exact component={HomePage} />
-            <Route exact path='/leaderboard' component={Leaderboard} />
-            <Route exact path='/add' component={NewQuestion} />
-            <Route exact path='/question/:id' component={Poll} />
-            <Route exact path='/logout' component={Logout} />
-          </Fragment>
-      }
-      <Route component={NotFound} />
-    </Switch>
-  </div>
+  return (
+    <div className="container">
+      <Switch>
+        {
+          !isLogged ? <Route path='/' exact component={Login}/> :
+            <Fragment>
+              <NavBar />
+              <Route path='/' exact component={HomePage} />
+              <Route exact path='/leaderboard' component={Leaderboard} />
+              <Route exact path='/add' component={NewQuestion} />
+              <Route exact path='/question/:id' component={Poll} />
+              <Route exact path='/logout' component={Logout} />
+            </Fragment>
+        }
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  )
 }
 
 export default Routes;
