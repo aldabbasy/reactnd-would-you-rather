b81ec0bdae5e87f059ae94af7809b20cc585e6e0
AL-Laith Dababseh <45473839+aldabbasy@users.noreply.github.com>
2020-12-25 03:43:48 +0200

    Modify UI of Question.js

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,53 +1,64 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles'
-import ListItem from '@material-ui/core/ListItem'
-
-import ListItemText from '@material-ui/core/ListItemText'
-import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import FormGroup from '@material-ui/core/FormGroup'
+import InputLabel from '@material-ui/core/InputLabel'
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardContent from '@material-ui/core/CardContent'
 import Avatar from '@material-ui/core/Avatar'
-import Typography from '@material-ui/core/Typography'
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import Button from '@material-ui/core/Button'
 import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    maxWidth: 500,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    margin: '1.25%',
   },
   inline: {
     display: 'inline',
   },
+  viewPoll: {
+    margin: theme.spacing(3, 0, 0),
+  },
+  buttonDiv: {
+    display: 'flex',
+    justifyContent: 'center'
+  }
 }))
 
 
 function Question({ question, author }) {
     const classes = useStyles()
     return (
-        <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt={author.name} src={author.avatarURL} />
-        </ListItemAvatar>
-        <ListItemText
-          primary={author.name + " asks: Would You Rather?"}
-          secondary={
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
+        <Card className={classes.root}>
+          <CardHeader
+              avatar={
+                <Avatar alt={author.name} src={author.avatarURL} />
+              }
+              title= { author.name + " Asks:" }
+              subheader='Would You Rather?'
+              classes={{backgroundColor: 'red'}}
+          />
+          <CardContent>
+            <div>
+              <FormGroup>
+                <InputLabel>
                 {question.optionOne.text} or ...
-              </Typography>
-          }
-        />
-        <ListItemSecondaryAction>
-          <Link to={`/question/${question.id}`} style={{ textDecoration: 'none', color:'white' }} > 
-            <Button type="button" color="primary">
-              View Poll
-            </Button>
-          </Link>
-        </ListItemSecondaryAction>
-      </ListItem>
+                </InputLabel>
+              </FormGroup>
+              <div className={classes.buttonDiv}>
+                <Link to={`/question/${question.id}`} style={{ textDecoration: 'none', color:'white' }} >
+                  <Button className={classes.viewPoll} type="button" variant="outlined" color="primary">
+                  View Poll
+                  </Button>
+                </Link>
+              </div>
+            </div>
+          </CardContent>
+      </Card>
     )
 }
 
