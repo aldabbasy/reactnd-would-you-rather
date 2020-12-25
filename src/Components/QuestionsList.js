06c9245d3ffa702de0bc7405d47da799aacae285
AL-Laith Dababseh <45473839+aldabbasy@users.noreply.github.com>
2020-12-25 17:44:25 +0200

    fixed refresh poll.js bug

--- src/Components/QuestionsList.js
+++ src/Components/QuestionsList.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 
 import { makeStyles } from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
 import List from '@material-ui/core/List'
